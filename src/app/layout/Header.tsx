85d0e307a4ee58af644ef0bcc734e2066a19d8ed
m4muchu <muchu.pkd123@gmail.com>
2021-06-19 22:07:37 +0530

    Completed scss implementation

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -7,10 +7,11 @@ const HeaderLayout = () => {
   return (
     <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1">HOME</Menu.Item>
+        <Menu.Item key="2">WOMEN</Menu.Item>
+        <Menu.Item key="3">MEN</Menu.Item>
+        <Menu.Item key="4">ACCESSORIES</Menu.Item>
       </Menu>
     </Header>
   )
