61005a2cda1d29c4068627b53c12c139f688b640
stepinfwd <stepinfwd@gmail.com>
2021-06-20 22:08:17 +0530

    updates

--- src/utils/Star.jsx
+++ src/utils/Star.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+<<<<<<< HEAD
 const Star = props => {
   return (
     <label className="star">
@@ -24,6 +25,39 @@ const Star = props => {
       </svg>
     </label>
   )
+=======
+const Star = (props) => {
+
+    const changeGrade = (e) => {
+        props.changeGradeIndex(e.target.value);
+    }
+
+    return (
+        <label className="star">
+            <input
+                type="radio"
+                name="rating"
+                id={props.grade}
+                value={props.index}
+                className="stars_radio-input"
+                onClick={changeGrade}
+            />
+            <svg
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#393939"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                style={props.style}
+            >
+                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+            </svg>
+        </label>
+    );
+>>>>>>> updates
 }
 
 export default Star
